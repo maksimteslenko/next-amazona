65149dff2ac0d53fe1887b81691ff1150f786897
Maksim Teslenko <mte@impact.dk>
2022-06-30 12:41:32 +0200

    Header links

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Head from 'next/head';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import NextLink from 'next/link';
+import { AppBar, Toolbar, Typography, Link } from '@mui/material';
 import { Container } from '@mui/system';
 import useStyles from '../../utils/styles';
 
 export default function Layout({ children }) {
   const classes = useStyles();
-  console.log(classes);
   return (
     <div>
       <Head>
@@ -14,7 +14,20 @@ export default function Layout({ children }) {
       </Head>
       <AppBar position="static" className={classes.navbar}>
         <Toolbar>
-          <Typography>amazona</Typography>
+          <NextLink href="/" passHref>
+            <Link>
+              <Typography className={classes.brand}>amazona</Typography>
+            </Link>
+          </NextLink>
+          <div className={classes.grow}></div>
+          <div>
+            <NextLink href="/cart" passHref>
+              <Link>Cart</Link>
+            </NextLink>
+            <NextLink href="/login" passHref>
+              <Link>Login</Link>
+            </NextLink>
+          </div>
         </Toolbar>
       </AppBar>
       <Container className={classes.main}>{children}</Container>
